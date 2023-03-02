a35e73f481668794178f72a47fc30f2d3119ea11
NicholasConti <conti94@live.it>
2023-03-02 15:34:10 +0100

    created-elements-js

--- js/mian.js
+++ js/mian.js
@@ -1,1 +1,24 @@
 "use strict";
+
+//------------------
+//FUNCTIONS
+//------------------
+function myCreateEl(tagEl, classEl){
+    const element = document.createElement(tagEl);  //creazioen div class= cell
+    element.classList.add(classEl);
+    return element;
+}
+
+function appendElement(containerEl, sonEl){
+    containerEl.append(sonEl);
+}
+
+
+//------------------
+//MAIN
+//------------------
+const cellBoard = document.querySelector('.board');
+for(let i = 1; i <= 100; i++){
+    const createdElement = myCreateEl('div', 'cell');
+    appendElement(cellBoard, createdElement);
+}
